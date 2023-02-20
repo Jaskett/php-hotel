1b28ccd9d8c40f17f5e90c7095756498adf07f20
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-20 16:21:39 +0100

    Creazione Tabella

--- index.php
+++ index.php
@@ -46,20 +46,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuHkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-        foreach($hotels as $hotel) {
-            echo $name = $hotel["name"] . '<br>';
-            echo $description = $hotel["description"] . '<br>';
-            if($hotel["parking"] === true) {
-                echo 'Parking: Available' . '<br>';
-            } else {
-                echo 'Parking: Not Available' . '<br>';
-            }
-            echo 'Vote:' . ' ' . $vote = $hotel["vote"] . '<br>';
-            echo 'Distance to Center:' . ' ' . $distanceToCenter = $hotel["distance_to_center"] . ' ' . 'km' . '<br>';
-        }
-    ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Hotel Name</th>
+                <th scope="col">Hotel Description</th>
+                <th scope="col">Hotel Parking</th>
+                <th scope="col">Hotel Vote</th>
+                <th scope="col">Distance to center</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+                foreach($hotels as $hotel) {
+                    $name = $hotel["name"];
+                    $description = $hotel["description"];
+                    $parking = $hotel["parking"];
+                    $vote = $hotel["vote"];
+                    $distanceToCenter = $hotel["distance_to_center"];
+
+                    echo '<tr>';
+                    echo '<th scope="row">' . $name . '</th>';
+                    echo '<td>' . $description . '</td>';
+
+                    if($parking === true) {
+                        echo '<td>' . 'Available' . '</td>';
+                    } else {
+                        echo '<td>' . 'Not Available' . '</td>';
+                    }
+
+                    echo '<td>' . $vote . '</td>';
+                    echo '<td>' . $distanceToCenter . ' ' . 'km' . '</td>';
+                    echo '</tr>';
+                }
+            ?>
+        </tbody>
+    </table>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuHH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
