0cb48477538d1f11559d72f7e26b7500c757c589
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-20 16:31:49 +0100

    Esercizio completato

--- index.php
+++ index.php
@@ -50,8 +50,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuHkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-    <table class="table">
-        <thead>
+    <table class="table table-hover">
+        <thead class="table-dark">
             <tr>
                 <th scope="col">Hotel Name</th>
                 <th scope="col">Hotel Description</th>
@@ -63,35 +63,44 @@
 
         <tbody>
             <?php
-                foreach($hotels as $hotel) {
-                    $name = $hotel["name"];
-                    $description = $hotel["description"];
-                    $parking = $hotel["parking"];
-                    $vote = $hotel["vote"];
-                    $distanceToCenter = $hotel["distance_to_center"];
+            foreach ($hotels as $hotel) {
 
-                    echo '<tr>';
-                    echo '<th scope="row">' . $name . '</th>';
-                    echo '<td>' . $description . '</td>';
+                $name = $hotel["name"];
+                $description = $hotel["description"];
+                $parking = $hotel["parking"];
+                $vote = $hotel["vote"];
+                $distanceToCenter = $hotel["distance_to_center"];
+                ?>
+                <tr>
+                    <th scope="row">
+                        <?php echo $name ?>
+                    </th>
+                    <td>
+                        <?php echo $description ?>
+                    </td>
 
-                    if($parking === true) {
-                        echo '<td>' . 'Available' . '</td>';
-                    } else {
-                        echo '<td>' . 'Not Available' . '</td>';
-                    }
+                    <td>
+                        <?php echo $parking ? 'Parking Available' : 'Parking not Available' ?>
+                    </td>
 
-                    echo '<td>' . $vote . '</td>';
-                    echo '<td>' . $distanceToCenter . ' ' . 'km' . '</td>';
-                    echo '</tr>';
-                }
-            ?>
+                    <td>
+                        <?php echo $vote ?>
+                    </td>
+                    <td>
+                        <?php echo $distanceToCenter . ' km' ?>
+                    </td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuHH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-
+q8i/X+965DzO0rTDNEpUTHQoQUJMHLrErGJyHg89uy71MyuHH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-
+ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-
+ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
